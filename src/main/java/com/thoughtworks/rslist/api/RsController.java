a84826f850cdba94e4789975b717217e958b8b5b
kker <907765631@qq.com>
2020-09-15 21:05:28 +0800

    [Mamingjun]: implement getOneRsList

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.dto.RsEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -7,5 +10,12 @@ import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList = Arrays.asList(new RsEvent("第一条事件", "无分类"),
+          new RsEvent("第二条事件", "无分类"),
+          new RsEvent("第三条事件", "无分类"));
+
+  @GetMapping("/rs/event/{index}")
+  public RsEvent getOneRsEvent(@PathVariable int index) {
+    return rsList.get(index - 1);
+  }
 }
