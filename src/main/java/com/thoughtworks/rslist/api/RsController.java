e9115464f15e403993983bcddde611cfa5fc939a
kker <907765631@qq.com>
2020-09-15 21:33:02 +0800

    [Mamingjun]: implement addOneRsEvent

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -3,17 +3,26 @@ package com.thoughtworks.rslist.api;
 import com.thoughtworks.rslist.dto.RsEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<RsEvent> rsList = Arrays.asList(new RsEvent("第一条事件", "无分类"),
-          new RsEvent("第二条事件", "无分类"),
-          new RsEvent("第三条事件", "无分类"));
+  private List<RsEvent> rsList = initRsList();
+
+  private List<RsEvent> initRsList() {
+    List<RsEvent> tempRsList = new ArrayList<>();
+    tempRsList.add(new RsEvent("第一条事件", "无分类"));
+    tempRsList.add(new RsEvent("第二条事件", "无分类"));
+    tempRsList.add(new RsEvent("第三条事件", "无分类"));
+    return tempRsList;
+  }
 
   @GetMapping("/rs/event/{index}")
   public RsEvent getOneRsEvent(@PathVariable int index) {
@@ -27,5 +36,10 @@ public class RsController {
       return rsList;
     }
     return rsList.subList(start - 1, end);
+  }
+
+  @PostMapping("/rs/add/event")
+  public void addOneRsEvent(@RequestBody RsEvent rsEvent) {
+    rsList.add(rsEvent);
   }
 }
