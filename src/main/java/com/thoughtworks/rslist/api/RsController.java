6ca350dfbea8f4309f98d88ab767b8198f11098b
kker <907765631@qq.com>
2020-09-16 20:08:54 +0800

    [Mamingjun]: refactor RsController ResponseEntity

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.dto.RsEvent;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,31 +28,32 @@ public class RsController {
   }
 
   @GetMapping("/rs/event/{index}")
-  public RsEvent getOneRsEvent(@PathVariable int index) {
-    return rsList.get(index - 1);
+  public ResponseEntity<RsEvent> getOneRsEvent(@PathVariable int index) {
+    return ResponseEntity.ok().body(rsList.get(index - 1));
   }
 
   @GetMapping("/rs/event")
-  public List<RsEvent> getRsEventByRange(@RequestParam(required = false) Integer start,
+  public ResponseEntity<List<RsEvent>> getRsEventByRange(@RequestParam(required = false) Integer start,
                                          @RequestParam(required = false) Integer end) {
     if (start == null || end == null) {
-      return rsList;
+      return ResponseEntity.ok().body(rsList);
     }
-    return rsList.subList(start - 1, end);
+    return ResponseEntity.ok().body(rsList.subList(start - 1, end));
   }
 
   @PostMapping("/rs/add/event")
-  public void addOneRsEvent(@Valid @RequestBody RsEvent rsEvent) {
+  public ResponseEntity<Object> addOneRsEvent(@Valid @RequestBody RsEvent rsEvent) {
     if (!UserController.userList.contains(rsEvent.getUser())) {
       UserController.userList.add(rsEvent.getUser());
     }
     rsList.add(rsEvent);
+    return ResponseEntity.ok().build();
   }
 
   @PutMapping("/rs/update/event/{index}")
-  public String updateRsEventByIndex(@PathVariable Integer index, @RequestBody RsEvent rsEvent) {
+  public ResponseEntity<String> updateRsEventByIndex(@PathVariable Integer index, @RequestBody RsEvent rsEvent) {
     if (index == null || rsList.size() < index) {
-      return "更新失败";
+      return ResponseEntity.ok().body("更新失败");
     }
     RsEvent thisRsEvent = rsList.get(index - 1);
     if (rsEvent.getEventName() != null) {
@@ -60,15 +62,15 @@ public class RsController {
     if (rsEvent.getKeyword() != null) {
       thisRsEvent.setKeyword(rsEvent.getKeyword());
     }
-    return "更新成功";
+    return ResponseEntity.ok().body("更新成功");
   }
 
   @DeleteMapping("/rs/delete/event/{index}")
-  public String deleteRsEventByIndex(@PathVariable Integer index) {
+  public ResponseEntity<String> deleteRsEventByIndex(@PathVariable Integer index) {
     if (index == null || rsList.size() < index) {
-      return "删除失败";
+      return ResponseEntity.ok().body("删除失败");
     }
     rsList.remove(index - 1);
-    return "删除成功";
+    return ResponseEntity.ok().body("删除成功");
   }
 }
