bcb8ede54c7c5db72c3772fbdd380ec14e75c90b
kker <907765631@qq.com>
2020-09-21 09:34:06 +0800

    [Mamingjun]: refactor MyConfig

--- src/main/java/com/thoughtworks/rslist/config/MyConfig.java
+++ src/main/java/com/thoughtworks/rslist/config/MyConfig.java
@@ -3,21 +3,15 @@ package com.thoughtworks.rslist.config;
 import com.thoughtworks.rslist.repository.RsEventRepository;
 import com.thoughtworks.rslist.repository.UserRepository;
 import com.thoughtworks.rslist.service.RsEventService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfig {
 
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    RsEventRepository rsEventRepository;
-
     @Bean
-    public RsEventService rsEventService() {
+    public RsEventService rsEventService(UserRepository userRepository,
+                    RsEventRepository rsEventRepository) {
         return new RsEventService(rsEventRepository, userRepository);
     }
 }
